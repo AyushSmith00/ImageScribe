a1ff69e074c2a8ca4709da15758f3cb30c377303
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-04-16 18:25:31 +0530

    Fifth Commit

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -20,7 +20,7 @@ export class AuthService {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
 
             if (userAccount) {
-                return this.login({email, password})
+                return this.login({email, password});
                 
             } else {
                 return userAccount;
@@ -37,6 +37,24 @@ export class AuthService {
 
         } catch (error) {
             throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+
+        return null; 
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error;            
         }
     }
 }
