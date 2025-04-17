ecc6884d7c7a0cb81f4e9d63099489000a6d06c4
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-04-17 18:50:24 +0530

    Seventh Commit

--- src/appwrite/mainConfig.js
+++ src/appwrite/mainConfig.js
@@ -29,8 +29,106 @@ export class Service{
                 }
             )
         } catch (error) {
-            return error
+            console.log("Appwrite service :: getPosts :: error", error);
         }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug 
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false;
+        }
+    }
+    async getpost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    async getPosts(quaries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                quaries,
+
+            )
+            
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    //file upload service
+
+    async uploadFile(file) {
+        try {
+
+            return await this.buket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.buket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+                
+            )
+            return true
+
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.buket.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
     }
 }
 
