878f935d40021e2714c7d335716aa8ddf0e75108
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-04-23 12:58:36 +0530

    Updated Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,9 +14,43 @@ function Login(){
 
     const login = async(data) => {
         setError("")
+        try {
+            const session = await authService.login(data)
+            if (session){
+                const userData = await authService.getCurrentUser()
+                if (userData) dispatch(authLogin(userData));
+                navigate("/")
+            }
+            
+        } catch (error) {
+            setError(error.message)
+            
+        }
     }
     return(
-        <div></div>
+        <div className="felx items-center justify-center w-full">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl
+                p-10 border border-black/10`}>
+            <div className="mb-2 flex justify-center">
+                <span className="inline-block w-full max-w-[100px}">
+                    <Logo width="100%"></Logo>
+                </span>
+            </div>
+            <h2 className="text-center text-2xl font-bold leading-tight">
+                Sign in to your account 
+            </h2>
+            <p className="mt-2 text-center text-base text-black/60">
+            Don&apos;t have any account? &nbsp;
+            <Link
+            to="/signup"
+            className="font-medium text-primary transition-all duration-200
+            hover:underline"
+            >Sign Up</Link>
+            </p>
+            {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+            <form onSubmit={handelSubmit(login)}></form>
+            </div>
+        </div>
     )
 }
 export default Login
