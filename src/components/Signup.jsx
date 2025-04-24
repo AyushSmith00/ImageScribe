bc0737d65d16583f515622dbcbb15a1daa55f902
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-04-24 14:16:09 +0530

    Sixteenth Commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import authService from "../appwrite/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
 import { login } from "../store/authSlice";
 import {Button, Input, Logo} from "./index"
 import { useDispatch } from "react-redux";
@@ -46,6 +46,43 @@ function Signup() {
                 </p>
                 {error && <p className="text-red-600 mt-8
                 text-center">{error}</p>}
+
+                <form onSubmit={handelSubmit(create)}>
+                    <div className="space-y-5">
+                        <Input
+                        label = "Full Name: "
+                        placeholder = "Enter your full name"
+                        {...register("name", {
+                            required: true,
+                        })}
+                        />
+                        <Input
+                        label= "Email: "
+                        placeholder= "Enter your Email"
+                        type= "email"
+                        {...register("email", {
+                            required: true,
+                            validate: {
+                                matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                "Email address must be a valid address",
+                                    
+                            }
+                        })}
+                        />
+
+                        <Input
+                        label = "Password: "
+                        placeholder = "Enter your Password"
+                        type = "password"
+                        {...register("password", {
+                            required: true,
+                        })}
+                        />
+                        <Button
+                        type="submit"
+                        className="w-full">Create Account</Button>
+                    </div>
+                </form>
             </div>
         </div>
     )
