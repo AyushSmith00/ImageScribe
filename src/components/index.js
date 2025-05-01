aed53ef0fd76236a73c4a95c9d68e6628ff19b53
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-05-01 20:09:14 +0530

    Eighteenth Commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container";
+import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
@@ -10,7 +10,7 @@ import Signup from "./Signup";
 import Login from "./Login";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout";
+import Protected from "./AuthLayout"
 import RTE from "./RTE";
 
 export {
@@ -26,7 +26,7 @@ export {
     Login,
     PostForm,
     PostCard,
-    AuthLayout,
+    Protected,
     RTE,
     
 
---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, PostCard } from "../components";
+import appwriteService from "../appwrite/mainConfig"
+
+function AllPosts(){
+    return(
+        <div className="py-8">
+        </div>
+    )
+}
+
+export default AllPosts
