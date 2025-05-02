84cde520c9405e76479afbcb8527ce6e73c82c50
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-05-02 13:14:10 +0530

    Created AllPost.jsx

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Container, PostCard } from "../components";
 import appwriteService from "../appwrite/mainConfig"
 
 function AllPosts(){
+
+    const [posts, setPost] = useState([])
+    useEffect(() => {}, [])
+    appwriteService.getPosts([]).then((posts)=> {
+        if (posts) {
+            setPost(posts.documents)
+        }
+    })
+
     return(
-        <div className="py-8">
+        <div className="w-full py-8">
+            <Container>
+                <div className="flex flex-wrap">
+                    {posts.map((post) => (
+                        <div key={post.$id} className="p-2 w-1/4">
+                            <PostCard/>
+                        </div>
+                    ))}
+                </div>
+            </Container>
         </div>
     )
 }
